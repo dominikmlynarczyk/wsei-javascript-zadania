b8925387887cbb2dd0e813599c8fb8c5c8f83688
Młynarczyk Dominik <dominik.mlynarczyk@microsoft.wsei.edu.pl>
2019-10-06 12:33:30 +0200

    Zadanie 3

--- 1_Zadania/obowiązkowe/2_Tablice/js/app.js
+++ 1_Zadania/obowiązkowe/2_Tablice/js/app.js
@@ -19,3 +19,14 @@ function distFromAverage(tab) {
 let tab = [1,2,3,4,5,6,7]
 
 distFromAverage(tab);
+
+//Zadanie 1
+
+var favouriteFruits = ['apple', 'orange', 'bannana'];
+
+console.log(favouriteFruits[0]);
+console.log(favouriteFruits[favouriteFruits.length -1]);
+
+for (var i = 0; i < favouriteFruits.length; i++) {
+    console.log(favouriteFruits[i]);
+}
