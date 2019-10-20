b84b3264dcbafc8f1dfd4d0bf4652fcd6b7168dc
Młynarczyk Dominik <dominik.mlynarczyk@microsoft.wsei.edu.pl>
2019-10-20 12:50:45 +0200

    Zajęcia 2. Zadanie 5, tablice wielowymiarowe

--- 1_Zadania/obowiązkowe/3_Tablice_wielowymiarowe/js/app.js
+++ 1_Zadania/obowiązkowe/3_Tablice_wielowymiarowe/js/app.js
@@ -78,3 +78,21 @@ var task3Array = [
 ]
 
 print2DArray(task3Array);
+
+// Zadanie 5
+
+function create2DArray(rows, columns){
+    var counter = 1;
+    var array1 = [];
+    for(let i = 0; i < rows; i++) {
+        var array2 = [];
+        for(let j = 0; j < columns; j++) {
+            array2.push(counter);
+            counter++;
+        }
+        array1.push(array2);
+    }
+    return array1;
+}
+
+create2DArray(4, 4);
