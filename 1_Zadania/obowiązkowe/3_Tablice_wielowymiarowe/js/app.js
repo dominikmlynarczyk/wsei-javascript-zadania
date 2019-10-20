8547e976018f258b688fa25ce4a8c84b968df47e
Młynarczyk Dominik <dominik.mlynarczyk@microsoft.wsei.edu.pl>
2019-10-20 11:49:27 +0200

    Zajęcia 2. Zadanie 0, tablice wielowymiarowe

--- 1_Zadania/obowiązkowe/3_Tablice_wielowymiarowe/js/app.js
+++ 1_Zadania/obowiązkowe/3_Tablice_wielowymiarowe/js/app.js
@@ -13,3 +13,22 @@ var task2Array = [
     [5, 6, 7, 8],
     [9, 10, 11, 12]
 ];
+
+// Zadanie 0
+
+function checkArray(ddarray) {
+    var result = [];
+    for(let i = 0; i < ddarray.length; i++) {
+        const isNotOdd = ddarray[i].filter(number => number % 2 != 0).length > 0;
+        result.push(!isNotOdd);
+    }
+    return result;
+}
+
+checkArray([
+    [11, 12],
+    [42, 2],
+    [-4, -120],
+    [0, 0],
+    [1, 34],
+]);
