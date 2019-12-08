8af905ed7e66a4bdc0b8d1389f5922e80b72caa1
Młynarczyk Dominik <dominik.mlynarczyk@microsoft.wsei.edu.pl>
2019-12-08 19:13:46 +0100

    Zadania 9 i 10, do sprawdzenia jeszcze

--- 1_Zadania/obowiązkowe/10_DOM_Wiecej_o_elemencie/js/app.js
+++ 1_Zadania/obowiązkowe/10_DOM_Wiecej_o_elemencie/js/app.js
@@ -3,4 +3,33 @@ document.addEventListener("DOMContentLoaded", function () {
     Poniżej napisz kod rozwiązujący zadania
      */
 
+    // Zadanie 0
+    const ex5 = document.querySelectorAll(".ex5 li");
+    for(let i=0; i < ex5.length; i++) {
+        if(i === 0 || i % 2 === 0) {
+            ex5[i].style.backgroundColor = 'green';
+        }
+        if (i % 5 === 0) {
+            ex5[i].classList.add('big');
+        }
+        if (i % 3 === 0 ) {
+            ex5[i].style.textDecoration = "underline";
+        }
+    }
+
+    // Zadanie 1
+    const chromeLogo = document.querySelector('.chrome');
+    const firefoxLogo = document.querySelector('.firefox');
+    const edgeLogo = document.querySelector('.edge');
+
+    chromeLogo.style.backgroundRepeat = 'no-repeat';
+    firefoxLogo.style.backgroundImage = './assets/img/firefox.png'
+    edgeLogo.style.backgroundImage = 'url("./assets/img/edge.png");'
+    chromeLogo.style.width = '100px';
+    chromeLogo.nextElementSibling.innerHTML = 'Chrome';
+
+    chromeLogo.nextElementSibling.setAttribute('href', 'http://chrome.html');
+    firefoxLogo.nextElementSibling.setAttribute('href', 'http://firefox.html');
+    edgeLogo.nextElementSibling.setAttribute('href', 'http://edge.html')
+
 });
--- 1_Zadania/obowiązkowe/9_DOM_Element/js/app.js
+++ 1_Zadania/obowiązkowe/9_DOM_Element/js/app.js
@@ -4,9 +4,84 @@ document.addEventListener("DOMContentLoaded", function () {
     var banner = document.querySelector(".ban");
     var blocks = document.querySelectorAll(".block");
     var links = document.querySelector(".links").children;
+    // Zadanie 0
+    // function getDatasInfo(elements) {
+    //     var newArray = [];
+    //     for (let i = 0; i<elements.length; i++) {
+    //         newArray.push(elements[i].getAttibute('data-color'));
+    //     }
+    //     console.log(newArray);
+    //     return NewArray
+    // }
 
     /*
     Poniżej napisz kod rozwiązujący zadania
      */
+    // getDatasInfo(links);
 
+    // Zadanie 1
+
+
+
+    // Zadanie 2
+    for (el of blocks) {
+        console.log(el.innerHTML);
+        console.log(el.outerHTML);
+        el.innerHTML = "Nowa wartość diva o klasie blocks";
+        console.log(el.innerHTML);
+        console.log(el.outerHTML);
+    }
+
+
+    // Zadanie 3
+    var mainFooter = document.querySelector('#mainFooter');
+    function getID(element) {
+        console.log(element.id);
+        return element.id;
+    }
+
+    getID(mainFooter);
+
+    // Zadanie 4
+
+    function getTags(elements) {
+        let arr1 = [];
+
+        for (let i = 0; i < elements.length; i++) {
+            arr1.push(elements[i].tagName);
+        }
+
+        console.log(arr1);
+        return arr1;
+    }
+
+    getTags(childElements);
+
+    // Zadanie 5
+
+    function getClassInfo(elements) {
+        const newArray = [];
+        for (let i = 0; i < elements.classList.length; i++) {
+            newArray.push(elements.classList[i]);
+        }
+
+        console.log(newArray);
+        return newArray;
+    }
+
+    getClassInfo(banner);
+
+    // Zadanie 6
+
+    const liElements = document.querySelectorAll('nav li');
+
+    function setDataDirection(elements) {
+        for(item of elements) {
+            if(!item.getAttribute('data-direction')) {
+                item.setAttribute('data-direction', 'top');
+            }
+        }
+    }
+
+    setDataDirection(liElements);
 });
