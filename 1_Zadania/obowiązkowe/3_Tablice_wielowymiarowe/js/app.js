107c18c1e04642854238a3fd01ad372e2f6dbf95
Sonor Sound <40466173+sonorsound@users.noreply.github.com>
2020-02-01 22:07:34 +0100

    REFACTOR solutions to Zadania 3 & 4 - FINISHED

--- 1_Zadania/obowiązkowe/3_Tablice_wielowymiarowe/js/app.js
+++ 1_Zadania/obowiązkowe/3_Tablice_wielowymiarowe/js/app.js
@@ -1,98 +1,63 @@
+var task1Array = [ [ 2, 3 ], [ 'Ala', 'Ola' ], [ true, false ], [ 5, 6, 7, 8 ], [ 12, 15, 67 ] ];
 
-var task1Array = [
-    [2, 3],
-    ["Ala", "Ola"],
-    [true, false],
-    [5, 6, 7, 8],
-    [12, 15, 67]
-];
-
-
-var task2Array = [
-    [1, 2, 3, 4],
-    [5, 6, 7, 8],
-    [9, 10, 11, 12]
-];
+var task2Array = [ [ 1, 2, 3, 4 ], [ 5, 6, 7, 8 ], [ 9, 10, 11, 12 ] ];
 
 // Zadanie 0
-
-function checkArray(ddarray) {
-    var result = [];
-    for(let i = 0; i < ddarray.length; i++) {
-        const isNotOdd = ddarray[i].filter(number => number % 2 != 0).length > 0;
-        result.push(!isNotOdd);
-    }
-    return result;
+function checkArray(array) {
+	const result = [];
+	for (let i = 0; i < array.length; i++) {
+		const isNotOdd = array[i].filter((number) => number % 2 != 0).length > 0;
+		result.push(!isNotOdd);
+	}
+	return result;
 }
 
-checkArray([
-    [11, 12],
-    [42, 2],
-    [-4, -120],
-    [0, 0],
-    [1, 34],
-]);
+checkArray([ [ 11, 12 ], [ 42, 2 ], [ -4, -120 ], [ 0, 0 ], [ 1, 34 ] ]);
 
 // Zadanie 1
-
 console.log(task1Array[3][2]);
-
 console.log(task1Array[1].length);
-
 console.log(task1Array[4][2]);
 
 // Zadanie 2
+task2Array.forEach((el) => console.log(el));
+
+task2Array.forEach((el, index) => console.log(task2Array[index].length));
 
 for (let i = 0; i < task2Array.length; i++) {
-    console.log(task2Array[i]);
-}
-
-for (let i = 0; i < task2Array.length; i++) {
-    console.log(task2Array[i].length);
-}
-
-for (let i = 0; i < task2Array.length; i++) {
-    for (let j = 0; j < task2Array[i].length; j++) {
-        console.log(task2Array[i][j]);
-    }
+	for (let j = 0; j < task2Array[i].length; j++) {
+		console.log(task2Array[i][j]);
+	}
 }
 
 // Zadanie 3
-
-function print2DArray(task2Array) {
-    for (let i = 0; i < task2Array.length; i++) {
-        for (let j = 0; j < task2Array[i].length; j++) {
-            console.log(task2Array[i][j]);
-        }
-    }
+function print2DArray(array) {
+	array.forEach((el) => el.forEach((el2) => console.log(el2)));
 }
 
-print2DArray([[1,2], [3,4]]);
+print2DArray([ [ 1, 2 ], [ 3, 4 ] ]);
 
 // Zadanie 4
-
-var task3Array = [
-    [1, 2, 3, 4],
-    [5, 6, 7, 8],
-    [9, 10, 11, 12]
-]
+const task3Array = [ [ 1, 2, 3, 4 ], [ 5, 6, 7, 8 ], [ 9, 10, 11, 12 ] ];
 
 print2DArray(task3Array);
+// lub wypisanie całej tablicy:
+console.log(task3Array);
 
 // Zadanie 5
-
-function create2DArray(rows, columns){
-    var counter = 1;
-    var array1 = [];
-    for(let i = 0; i < rows; i++) {
-        var array2 = [];
-        for(let j = 0; j < columns; j++) {
-            array2.push(counter);
-            counter++;
-        }
-        array1.push(array2);
-    }
-    return array1;
+function create2DArray(rows, columns) {
+	let counter = 1;
+	const array = [];
+	for (let i = 0; i < rows; i++) {
+		array[i] = [];
+		for (let j = 0; j < columns; j++) {
+			array[i][j] = counter;
+			counter++;
+		}
+	}
+	return array;
 }
 
-create2DArray(4, 4);
+const newArray = create2DArray(4, 4);
+// Dodatkowe wypisanie w konsoli elementów tablicy
+newArray.forEach((el) => console.log(el));
--- 1_Zadania/obowiązkowe/4_Obiekty/js/app.js
+++ 1_Zadania/obowiązkowe/4_Obiekty/js/app.js
@@ -1,44 +1,43 @@
-// Zadanie 0
-
-const city = {                
-    capital: "Warszawa",          
-    population: 2000000,
-    president: "Trzaskowski",
-    primeMinisters: ["Adam", "Bogdan"]
-}; 
-
-console.log(city.capital + city.population + city.president + city.primeMinisters);
-
-const timeMachine = {
-    shape: "Portal",
-    model: "",
-    run : function (date, place) {
-        console.log (`Przeniosłeś się do ${place} i jest obecnie ${date}`);
-    }
+// Zadanie 0a
+const city = {
+	capital: 'Warszawa',
+	population: 1777972,
+	president: 'Rafał Trzaskowski',
+	primeMinisters: [ 'Jarek', 'Antek' ]
 };
 
-timeMachine.run("20-11-2030", "Sydney");
+console.log(
+	`Stolica: ${city.capital}; Populacja: ${city.population}; Prezydent: ${city.president}; Prime Ministers: ${city.primeMinisters}`
+);
+
+// Zadanie 0b
+const timeMachine = {
+	shape: 'Portal',
+	model: 'XJS',
+	run: function(date, place) {
+		console.log(`Przeniosłeś się do ${place} i jest obecnie ${date}`);
+	}
+};
+console.log(timeMachine.shape);
+console.log(timeMachine.model);
+timeMachine.run('20-11-2030', 'Sydney');
 
 // Zadanie 1
-
 const book = {
-    title: "test",
-    author: "author",
-    numberOfPages: "number"
+	title: 'Wiedźmin - Krew Elfów',
+	author: 'Andrzej Sapkowski',
+	numberOfPages: 340
 };
 
-for (const key of book) {
-    console.log(book[key]);
-}
+console.log(`Tytuł: ${book.title}; Autor: ${book.author}; Ilość stron: ${book.numberOfPages}`);
 
 // Zadanie 2
+const person = {};
 
-var person = {
-    name: "Andrzej",
-    age: 12,
-    sayHello: function() {
-        console.log("hello");
-    }
+person.name = 'Andrzej';
+person.age = 12;
+person.sayHello = function() {
+	console.log('hello');
 };
 
 console.log(person.name);
@@ -46,92 +45,99 @@ console.log(person.age);
 person.sayHello();
 
 // Zadanie 3
-
 const recipe = {
-    title = 'przepis',
-    servings = 0
+	title: 'Przepis na jajecznicę',
+	servings: 0
 };
 
 recipe.ingredients = [];
+// Dodatkowa metoda dodająca składniki
 recipe.addIngredient = function(ingredient) {
-    this.ingredient.push(ingredient);
-    this.servings++;
+	this.ingredient.push(ingredient);
+	this.servings++;
 };
 
-recipe.addIngredient("Składnik 1");
-recipe.addIngredient("Składnik 2");
+recipe.addIngredient('Składnik 1');
+recipe.addIngredient('Składnik 2');
 
 console.log(recipe.title);
 console.log(recipe.servings);
 console.log(recipe.ingredients);
 
-
 // Zadanie 4
-
 var movie = {
-    director: "Peter Jackson",
-    writers: [" J.R.R. Tolkien ", "Fran Walsh", "Philippa Boyens", "Peter Jackson"],
-    stars: ["Elijah Wood", "Ian McKellen", "Orlando Bloom"],
-    country: "New Zealand | USA",
-    budget: "$93,000,000",
-    imdbPoints: 8.8,
-    year: 2001,
-    time: "2h 58 min",
-    category: " Adventure, Drama, Fantasy"
-}
+	director: 'Peter Jackson',
+	writers: [ ' J.R.R. Tolkien ', 'Fran Walsh', 'Philippa Boyens', 'Peter Jackson' ],
+	stars: [ 'Elijah Wood', 'Ian McKellen', 'Orlando Bloom' ],
+	country: 'New Zealand | USA',
+	budget: '$93,000,000',
+	imdbPoints: 8.8,
+	year: 2001,
+	time: '2h 58 min',
+	category: ' Adventure, Drama, Fantasy'
+};
 
 for (const show in movie) {
-    console.log(moive[show]);
+	console.log(show + ': ' + movie[show.valueOf()]);
 }
 
-// Zadanie 5 
-
+// Zadanie 5
 var animals = [
-    {
-        type: "cat",
-        breed: "persian",
-        name: "Tiberius",
-        health: [
-            {
-                date: "2012-03-03",
-                visitType: "grafting"
-            },
-            {
-                date: "2015-06-23",
-                visitType: "bowel surgery"
-            }
-        ]
-    },
-    {
-        type: "Guinea pig",
-        breed: "The Rex",
-        name: "Marko",
-        health: [
-            {
-                date: "2015-12-04",
-                visitType: "grafting"
-            },
-            {
-                date: "2016-03-15",
-                visitType: "ear cleaning"
-            }
-        ]
-    }
+	{
+		type: 'cat',
+		breed: 'persian',
+		name: 'Tiberius',
+		health: [
+			{
+				date: '2012-03-03',
+				visitType: 'grafting'
+			},
+			{
+				date: '2015-06-23',
+				visitType: 'bowel surgery'
+			}
+		]
+	},
+	{
+		type: 'Guinea pig',
+		breed: 'The Rex',
+		name: 'Marko',
+		health: [
+			{
+				date: '2015-12-04',
+				visitType: 'grafting'
+			},
+			{
+				date: '2016-03-15',
+				visitType: 'ear cleaning'
+			}
+		]
+	}
+];
 
-]
-
-for (let index = 0; index = animal.length; index++) {
-    for (const key in animal[index]) {
-        console.log(animals[index][key]);
-    }
+for (let i = 0; i < animals.length; i++) {
+	for (let property in animals[i]) {
+		if (!Array.isArray(animals[i][property.valueOf()])) {
+			console.log(`${property}: ${animals[i][property.valueOf()]}`);
+		} else {
+			for (let health in animals[i][property.valueOf()]) {
+				for (let innerProperty in animals[i][property.valueOf()][health]) {
+					console.log(
+						`${innerProperty.valueOf()}: ${animals[i][property.valueOf()][health][innerProperty.valueOf()]}`
+					);
+				}
+			}
+		}
+	}
 }
 
 // Zadanie 6
-
 var spoon = {
-    isExist: true
-}
+	isExist: true
+};
 
 var fork = spoon;
-fork.isExist  = false;
+fork.isExist = false;
+
+// False - Łyżka nie istnieje :)
 console.log(spoon.isExist);
